ce42aef30ae75c55d2e2b5ddaf2f4890a4bf84e3
CallMeInk <907022926@qq.com>
2016-05-09 21:59:47 +0800

    Update AbstractFather.java

--- helloworld/src/date20160509/AbstractFather.java
+++ helloworld/src/date20160509/AbstractFather.java
@@ -2,7 +2,7 @@ package date20160509;
 
 public abstract class AbstractFather {
 
-	private String name = "jibajie";
+	private String name = "修改了这里！！！";
 	
 	public String getName() {
 		return name;
