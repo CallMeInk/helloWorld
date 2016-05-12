c34e8dfec66bc1f15e09c5a1c07276425a36477f
CallMeInk <907022926@qq.com>
2016-05-12 21:41:15 +0800

    日期类

---
+++ helloworld/src/number26/TestCalendar.java
@@ -0,0 +1,11 @@
+package number26;
+
+import java.util.Calendar;
+
+public class TestCalendar {
+
+	public static void main(String[] args) {
+		Calendar calendar = Calendar.getInstance();
+		System.out.println(calendar.get(calendar.YEAR));
+	}
+}
---
+++ helloworld/src/number26/TestDate.java
@@ -0,0 +1,12 @@
+package number26;
+
+import java.util.Date;
+
+public class TestDate {
+	
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println("date:"+date);
+	}
+
+}
